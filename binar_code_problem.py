0226e26b66b99df41cbabcb5b167f880e1b313dd
skarabayev <skarabayev@gmail.com>
2020-05-20 00:27:35 +0500

    Create binar_code_problem.py
    Binar problem in russian.

---
+++ binar_code_problem.py
@@ -0,0 +1,57 @@
+# https://repl.it/@Levashov/GuessNumberGame
+import time
+# Настройки
+a = 0
+b = 100
+print(f'Загадай число от {a} до {b}...')
+
+# Определим свою функцию, которая напечатает правила
+def print_rules():
+    rules = '''Если я угадаю, напиши "=",
+      если твое число меньше, то введи "<",
+      а если больше, то ">".
+      И нажми на Enter.'''
+    print()  # Пустая строка
+    print(rules)
+    print()
+
+# Даем время на подумать
+
+time.sleep(1)
+print('Загадал?')
+time.sleep(1)
+print('Тогда поехали!')
+time.sleep(1)
+print_rules()
+time.sleep(1)
+
+# Отгадываем
+steps = 0
+
+while True:
+    if a > b:
+        print('Произошел конфуз...')
+        print('Попробуй заново')
+        break
+    if a == b:
+        print(f'Все понятно. Ответ - {a}')
+        print(f'Потребовалось шагов: {steps}')
+        break
+
+    steps += 1
+
+    number = (a + b) // 2
+    answer = input(f'Это {number}? ')
+
+    if answer == '=':
+        print('Ура! Я отгадал!')
+        print(f'Потребовалось шагов: {steps}')
+        break
+    elif answer == '<':
+        b = number - 1
+    elif answer == '>':
+        a = number + 1
+    else:
+        print('Не понял. На всякий случай напомню правила:')
+        print_rules()
+        steps -= 1
